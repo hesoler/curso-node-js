61ca83368e6b13742171b605da7048c220337f4d
Héctor Soler <hecalana@gmail.com>
2024-07-27 01:50:47 -0400

    Add class #3

---
+++ class-3/schemas/movies.js
@@ -0,0 +1,36 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string.',
+    required_error: 'Movie title is required.'
+  }),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10),
+  poster: z.string().url({
+    message: 'Poster must be a valid url'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Comedy', 'Crime', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+    {
+      required_error: 'Movie genre is required.',
+      invalid_type_error: 'Movie genre must be an array of enum Genre.'
+    }
+  )
+})
+
+function validateMovie (object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie (input) {
+  // todas las props son opcionales pero las que aparezcan deben validarse
+  return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
