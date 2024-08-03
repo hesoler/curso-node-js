7b34283072f072c590b29c0c13733adb13676ad9
Héctor Soler <hecalana@gmail.com>
2024-08-03 00:48:58 -0400

    Add class #4

---
+++ class-4/middlewares/cors.js
@@ -0,0 +1,16 @@
+import cors from 'cors'
+
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'http://movies.com'
+]
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
+  origin: (origin, callback) => {
+    if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+      return callback(null, true)
+    }
+    return callback(new Error('Not allowed by CORS'))
+  }
+})
---
+++ class-4/utils.js
@@ -0,0 +1,4 @@
+// forma recomendada para leer JSON
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url)
+export const readJSON = (path) => require(path)
