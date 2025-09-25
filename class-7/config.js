60962a150eddbbba3d89e5a187e206c5aea481f4
Héctor Soler <hecalana@gmail.com>
2025-09-25 01:58:24 -0300

    feat: Implement user authentication with JWT and add login/register forms

--- class-7/config.js
+++ class-7/config.js
@@ -1,4 +1,5 @@
 export const {
   PORT = 3000,
-  SALT_ROUNDS = 10
+  SALT_ROUNDS = 10,
+  JWT_SECRET = 'this-is-an-awesome-secret-key-1234567890'
 } = process.env
--- class-7/index.js
+++ class-7/index.js
@@ -1,12 +1,32 @@
 import express from 'express'
-import { PORT } from './config.js'
+import { JWT_SECRET, PORT } from './config.js'
 import { UserRepository } from './user-repository.js'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 
 const app = express()
+
+app.set('view engine', 'ejs')
+
 app.use(express.json())
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  req.session = { user: null }
+
+  try {
+    const data = jwt.verify(token, JWT_SECRET)
+    req.session.user = data
+  } catch {}
+
+  next()
+})
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  const { user } = req.session
+  // Al usar EJS, se busca en la carpeta "views" por defecto
+  res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
@@ -17,6 +37,18 @@ app.post('/login', async (req, res) => {
 
   try {
     const user = await UserRepository.login({ username, password })
+    const token = jwt.sign({ id: user._id, username }, JWT_SECRET, { expiresIn: '1h' })
+    const refreshToken = jwt.sign({ id: user._id, username }, JWT_SECRET, { expiresIn: '7d' })
+
+    // TODO: guardar el refreshToken en la base de datos o en memoria (opcional)
+
+    // Enviar el token como una cookie segura
+    res.cookie('access_token', token, {
+      httpOnly: true, // No se puede acceder a la cookie desde JavaScript, solo desde el servidor
+      secure: process.env.NODE_ENV === 'production', // Solo se envía en HTTPS
+      sameSite: 'strict', // No se envía en solicitudes cross-site, es accesible solo en el mismo dominio
+      maxAge: 1000 * 60 * 60 // la cookie tiene validez durante 1 hora
+    })
     res.status(200).json({ user })
   } catch (error) {
     res.status(400).json({ error: error.message })
@@ -37,9 +69,16 @@ app.post('/register', async (req, res) => {
   }
 })
 
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+  res.clearCookie('access_token')
+  res.status(200).json({ message: 'Logged out successfully' })
+})
 
-app.post('/protected', (req, res) => {})
+app.get('/protected', (req, res) => {
+  const { user } = req.session
+  if (!user) return res.status(403).send('Access denied')
+  res.render('protected', user) // { _id, username }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
